3633ccc59f32351a1aaabde6715ef21e7e932ce3
onchari wycliffe <onchariwycliffe.qa@gmail.com>
2019-01-17 16:17:17 +0300

    Add project files.

---
+++ eshopper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eshopper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

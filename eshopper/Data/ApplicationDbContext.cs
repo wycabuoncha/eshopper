77a102e33169edcabfdb140b32448f3e4b51a8b7
onchari wycliffe <onchariwycliffe.qa@gmail.com>
2019-01-17 17:27:23 +0300

    Added shopping products

--- eshopper/Data/ApplicationDbContext.cs
+++ eshopper/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using eshopper.Models;
 
 namespace eshopper.Data
 {
@@ -12,5 +13,6 @@ namespace eshopper.Data
             : base(options)
         {
         }
+        public DbSet<eshopper.Models.Product> Product { get; set; }
     }
 }
---
+++ eshopper/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eshopper.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Photo { get; set; }
+
+    }
+}
